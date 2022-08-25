ae32979c8b689282c2ee96edf99a7fc117578820
Joan Jeremiah J <joanjeremiah04@gmail.com>
2022-08-25 14:44:24 +0530

    backend deploy preparation

--- backend/app.js
+++ backend/app.js
@@ -36,8 +36,8 @@ const server =  app.listen(PORT,()=>{
 
 const io = socket(server,{
   cors:{
-     origin:"http://localhost:3000",
-      Credential:true,     
+     origin:process.env.CORS_ORIGIN,
+     Credential:true,     
   }}
   );
 
